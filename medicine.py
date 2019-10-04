82e70dbd0c2dddb66c9f5507852b43aa04260c36
Abiess <essoudati@hotmail.de>
2019-10-04 16:38:56 +0200

    create medicine.py

---
+++ medicine.py
@@ -0,0 +1,36 @@
+from Setup import db, ma
+class medicine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    deadline = db.Column(db.DateTime)
+    consumption = db.Column(db.Integer )
+    amount = db.Column(db.Integr)
+	Kind	= db.Column(db.String)    
+    totalamount = db.Column(db.Integer)
+   
+
+
+    def __init__(self, name, deadline, consumption , amount, kind, totalamount):
+        self.name = name
+        self.deadline = deadline
+        self.consumption = consumption
+        self.amount = amount
+        self.kind = kind
+        self.totalamount = totalamount
+     
+
+
+class PatientSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('name', 'deadline', 'weight', 'consumption', 'amount', 'kind','totalamount')
+
+
+medicine_schema = MedicineSchema()
+medicines_schema = MedicineSchema(many=True)
+
+
+
+
+
+
