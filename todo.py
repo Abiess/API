0b27055e610bd46b81d08ff7905867c87993b558
Abiess <essoudati@hotmail.de>
2019-10-04 14:31:43 +0200

    add TodoStatus field to Todo Model

--- todo.py
+++ todo.py
@@ -8,9 +8,10 @@ class Todo(db.Model):
     AssignedTo = db.Column(db.String)
     Attachement = db.Column(db.String)
     Username = db.Column(db.String)
+    TodoStatus = db.Column(db.String)
 
 
-    def __init__(self, User_id, Subject, Description,CreationTime , AssignedTo, Attachement, Username):
+    def __init__(self, User_id, Subject, Description,CreationTime , AssignedTo, Attachement, Username, TodoStatus):
         self.User_id = User_id
         self.Subject = Subject
         self.Description = Description
@@ -18,12 +19,13 @@ class Todo(db.Model):
         self.AssignedTo = AssignedTo
         self.Attachement = Attachement
         self.Username = Username
+        self.TodoStatus = TodoStatus
 
 
 class TodoSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('User_id', 'Subject', 'Description', 'AssignedTo', 'Attachement', 'Username', 'CreationTime')
+        fields = ('User_id', 'Subject', 'Description', 'AssignedTo', 'Attachement', 'Username', 'CreationTime', 'TodoStatus')
 
 
 todo_schema = TodoSchema()
