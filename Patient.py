be8361ddf8de2f6fc7612341b81df671df5498ec
Abiess <essoudati@hotmail.de>
2019-10-04 16:50:49 +0200

    addd user id to model patient

--- Patient.py
+++ Patient.py
@@ -1,29 +1,31 @@
 from Setup import db, ma
 class Pateient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     firstname = db.Column(db.String)
     secondname = db.Column(db.String)
     weight = db.Column(db.Integer )
     gender = db.Column(db.String)
-	illness	 = db.Column(db.String)    
+    illnes = db.Column(db.String)    
     birthday = db.Column(db.Date)
    
 
 
-    def __init__(self, firstname, secondname, weight , gender, illness, birthday):
+    def __init__(self, User_id,  firstname, secondname, weight , gender, illness, birthday):
         self.firstname = firstname
         self.secondname = secondname
         self.weight = weight
         self.gender = gender
         self.illness = illness
         self.birthday = birthday
+	self.User_id = User_id
      
 
 
 class PatientSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('firstname', 'secondname', 'weight', 'gender', 'illness', 'birthday')
+        fields = ('User_id','firstname', 'secondname', 'weight', 'gender', 'illness', 'birthday')
 
 
 patient_schema = PatientSchema()
